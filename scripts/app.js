4babfbb572ed65e1b3316a053bb4eebdaa4f5ae0
Wahyu Febrianto <febriantowahyu63@gmail.com>
2020-12-24 11:13:21 +0700

    add profile name

--- scripts/app.js
+++ scripts/app.js
@@ -4,3 +4,17 @@ const loginBtn = document.querySelector('#login')
 loginBtn.addEventListener('click', () => {
   return liff.login()
 })
+
+// user logged
+const profileName = document.querySelector('#profile-name');
+
+if(liff.isLogin()){
+  profileData()
+}
+
+const profileData = async () => {
+  const profile = await liff.getProfile
+  const name = profile.displayName
+
+  profileName.innerHTML = name
+}
