8347d71ad7a74ee83801f963fbaa4ff2bbb5bffd
Wahyu Febrianto <febriantowahyu63@gmail.com>
2020-12-24 16:38:41 +0700

    load list data

--- scripts/liff-starter.js
+++ scripts/liff-starter.js
@@ -5,7 +5,6 @@ const cartComponent = document.querySelector('#cart')
 const btnHome = document.querySelector('#btn-home')
 const btnCart = document.querySelector('#btn-cart')
 
-
 let route = localStorage.getItem('ROUTE') || 'home'
 localStorage.setItem('ROUTE', route);
 
@@ -15,6 +14,7 @@ window.onload = () => {
   init(liffId)
 }
 
+// ROUTE COMPONENT
 const routeMode = () => {
   if(route === 'home') {
     homeComponentActive()
@@ -22,6 +22,9 @@ const routeMode = () => {
     cartComponentActive()
   }
 }
+
+
+// INITIALIZE LIFF
 const init = async (liffId) => {
   try {
     const initialize = await liff.init({ liffId })
@@ -30,29 +33,35 @@ const init = async (liffId) => {
     console.error(error)
   }
 }
-const wrapper = document.querySelector('#wrapper')
+
+
+// AUTHENTICATION SECTION
 const loginSection = document.querySelector('#login-section')
-
-const initializeApp = () => {
-  if(liff.isLoggedIn()) {
-    loginSection.classList.add('hidden')
-    wrapper.classList.remove('hidden')
-    profileData()
-    loader.classList.add('hidden')
-  }else{
-    loginSection.classList.add('block')
-    wrapper.classList.add('hidden')
-    loader.classList.add('hidden')
-  }
-}
-
-// LOGIN SECTION
 const loginBtn = document.querySelector('#login')
 
 loginBtn.addEventListener('click', () => {
   return liff.login()
 })
 
+
+// INITIALIZE APPLICATION
+const wrapper = document.querySelector('#wrapper')
+const initializeApp = () => {
+  if(liff.isLoggedIn()) {
+    loginSection.classList.add('hidden')
+    wrapper.classList.remove('hidden')
+
+    profileData()
+    loadListMenu()
+    // Disable loader
+    loader.classList.add('hidden')
+  }else{
+    loginSection.classList.add('block')
+    wrapper.classList.add('hidden')
+    // Disable loader
+    loader.classList.add('hidden')
+  }
+}
 
 
 // PROFILE SECTION
@@ -79,6 +88,37 @@ const profileData = async () => {
   } catch (error) {
     console.error(error)
   }
+}
+
+// LIST MENU SECTION
+const loadListMenu = () => {
+  let listData = null
+  const listMenu = [
+    { id:1, name: 'Strawberry Bubble Tea', price:60000, image_url:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmabrocteas.com%2Fmabrocbubbletea.com%2FProducts%2Fimages%2Fproducts%2FMBT_Bubbleshake_Strawberry.jpg&f=1&nofb=1' },
+    { id:2, name: 'Thai Iced Milk Tea', price:50000, image_url:'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0234%2F6273%2Fproducts%2Fthai_tea_buddha_bubbles_boba_a_large.jpg%3Fv%3D1429819336&f=1&nofb=1' },
+    { id:3, name: 'Honeydew Bubble Tea', price:35000, image_url:'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F1161%2F4842%2Fproducts%2Fhoneydew_bubble_tea_2_grande.jpg%3Fv%3D1479238222&f=1&nofb=1' },
+    { id:4, name: 'Mango Bubble Tea', price:30000, image_url:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fe8%2Fa0%2F6d%2Fe8a06d1411fd7971493a1180d54ffdcf.jpg&f=1&nofb=1' },
+  ]
+
+  for (const menu in listMenu) {
+    listData += `
+    <div class="flex justify-between items-center shadow bg-white rounded-lg mb-4 px-4 py-2">
+      <div class="flex justify-center items-center space-x-4">
+        <img class="w-16 h-16 object-cover" src="${menu.image_url}" alt="${menu.name}">
+        <div class="my-2">
+          <h4 class="text-lg">${menu.name}</h4>
+          <h4 class="text-sm">${menu.price}</h4>
+        </div>
+      </div>
+      <div >
+        <button class="w-8 h-8 text-red-500 bg-red-200 m-2 flex items-center justify-center rounded-lg">
+          <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd"></path></svg>
+        </button>
+      </div>
+    </div>
+    `
+  }
+  homeComponent.innerHTML = listData
 }
 
 // HANDLE BUTTON NAVIGATION BAR
