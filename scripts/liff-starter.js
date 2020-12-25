163d7a4ebd8d4b2292560dae0a2b3443eddbb64d
Wahyu Febrianto <febriantowahyu63@gmail.com>
2020-12-25 14:51:44 +0700

    load cart

--- scripts/liff-starter.js
+++ scripts/liff-starter.js
@@ -1,6 +1,7 @@
 const loader = document.querySelector('#loader')
 const homeComponent = document.querySelector('#home')
 const cartComponent = document.querySelector('#cart')
+const totalItems = document.querySelector('#total-items');
 
 const btnHome = document.querySelector('#btn-home')
 const btnCart = document.querySelector('#btn-cart')
@@ -16,9 +17,9 @@ window.onload = () => {
 
 // ROUTE COMPONENT
 const routeMode = () => {
-  if(route === 'home') {
+  if (route === 'home') {
     homeComponentActive()
-  }else{
+  } else {
     cartComponentActive()
     loadCartData()
   }
@@ -28,7 +29,9 @@ const routeMode = () => {
 // INITIALIZE LIFF
 const init = async (liffId) => {
   try {
-    const initialize = await liff.init({ liffId })
+    const initialize = await liff.init({
+      liffId
+    })
     await initializeApp()
   } catch (error) {
     console.error(error)
@@ -48,7 +51,7 @@ loginBtn.addEventListener('click', () => {
 // INITIALIZE APPLICATION
 const wrapper = document.querySelector('#wrapper')
 const initializeApp = () => {
-  if(liff.isLoggedIn()) {
+  if (liff.isLoggedIn()) {
     // loginSection.classList.add('hidden')
     // wrapper.classList.remove('hidden')
 
@@ -56,7 +59,7 @@ const initializeApp = () => {
     loadListMenu()
     // Disable loader
     loader.classList.add('hidden')
-  }else{
+  } else {
     // loginSection.classList.add('block')
     // wrapper.classList.add('hidden')
     // Disable loader
@@ -92,14 +95,43 @@ const profileData = async () => {
 }
 
 // LIST MENU SECTION
-const listMenu = [
-    { id:1, name: 'Strawberry Bubble Tea', price:60000, image_url:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2Fee%2Ffd%2Fd8%2Feefdd8f39672b0d86f6f19dc53bbed1a--strawberry-tea-milk-tea.jpg&f=1&nofb=1' },
-    { id:2, name: 'Thai Iced Milk Tea', price:50000, image_url:'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0234%2F6273%2Fproducts%2Fthai_tea_buddha_bubbles_boba_a_large.jpg%3Fv%3D1429819336&f=1&nofb=1' },
-    { id:3, name: 'Honeydew Bubble Tea', price:35000, image_url:'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F1161%2F4842%2Fproducts%2Fhoneydew_bubble_tea_2_grande.jpg%3Fv%3D1479238222&f=1&nofb=1' },
-    { id:4, name: 'Mango Bubble Tea', price:30000, image_url:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fe8%2Fa0%2F6d%2Fe8a06d1411fd7971493a1180d54ffdcf.jpg&f=1&nofb=1' },
-    { id:4, name: 'Mango Bubble Tea', price:30000, image_url:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fe8%2Fa0%2F6d%2Fe8a06d1411fd7971493a1180d54ffdcf.jpg&f=1&nofb=1' },
-    { id:4, name: 'Mango Bubble Tea', price:30000, image_url:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fe8%2Fa0%2F6d%2Fe8a06d1411fd7971493a1180d54ffdcf.jpg&f=1&nofb=1' },
-  ]
+const listMenu = [{
+    id: 1,
+    name: 'Strawberry Bubble Tea',
+    price: 60000,
+    image_url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2Fee%2Ffd%2Fd8%2Feefdd8f39672b0d86f6f19dc53bbed1a--strawberry-tea-milk-tea.jpg&f=1&nofb=1'
+  },
+  {
+    id: 2,
+    name: 'Thai Iced Milk Tea',
+    price: 50000,
+    image_url: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0234%2F6273%2Fproducts%2Fthai_tea_buddha_bubbles_boba_a_large.jpg%3Fv%3D1429819336&f=1&nofb=1'
+  },
+  {
+    id: 3,
+    name: 'Honeydew Bubble Tea',
+    price: 35000,
+    image_url: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F1161%2F4842%2Fproducts%2Fhoneydew_bubble_tea_2_grande.jpg%3Fv%3D1479238222&f=1&nofb=1'
+  },
+  {
+    id: 4,
+    name: 'Mango Bubble Tea',
+    price: 30000,
+    image_url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fe8%2Fa0%2F6d%2Fe8a06d1411fd7971493a1180d54ffdcf.jpg&f=1&nofb=1'
+  },
+  {
+    id: 4,
+    name: 'Mango Bubble Tea',
+    price: 30000,
+    image_url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fe8%2Fa0%2F6d%2Fe8a06d1411fd7971493a1180d54ffdcf.jpg&f=1&nofb=1'
+  },
+  {
+    id: 4,
+    name: 'Mango Bubble Tea',
+    price: 30000,
+    image_url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fe8%2Fa0%2F6d%2Fe8a06d1411fd7971493a1180d54ffdcf.jpg&f=1&nofb=1'
+  },
+]
 
 const loadListMenu = () => {
   let listData = ''
@@ -133,23 +165,23 @@ const addToCart = (id) => {
 
   const itemId = listMenu.find(item => item.id === id)
 
-  if(!cartStorage) {
+  if (!cartStorage) {
     itemId.qty = 1
     itemId.subtotal = itemId.price * itemId.qty
 
     cart.push(itemId)
     localStorage.setItem('CART', [JSON.stringify(cart)])
-  }else{
+  } else {
     const cartData = JSON.parse(cartStorage)
     const checkItemExist = cartData.find(item => item.id === id)
-    if(checkItemExist) {
+    if (checkItemExist) {
       const indexItemId = cartData.findIndex(item => item.id === id);
       const itemId = cartData[indexItemId]
       itemId.qty = itemId.qty + 1
       itemId.subtotal = itemId.price * itemId.qty
       cart = cartData
       localStorage.setItem('CART', [JSON.stringify(cartData)])
-    }else{
+    } else {
       itemId.qty = 1
       itemId.subtotal = itemId.price * itemId.qty
 
@@ -159,15 +191,76 @@ const addToCart = (id) => {
   }
 }
 
+const cartHeader = document.querySelector('cart-header')
+const cartItems = document.querySelector('cart-items')
+
 const loadCartData = () => {
   cartData = localStorage.getItem('CART') || []
   const data = cart.length !== 0 ? cart : cartData
 
-  if(data.length !== 0){
+  const headerData = ''
+  const itemsData = ''
 
-  }else{
+  if (data.length !== 0) {
+    headerData +=
+      `
+      <div class="mb-4">
+        <div class="text-xl font-semibold text-purple-400 mb-2 flex space-x-2">
+          <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd"
+              d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
+              clip-rule="evenodd">
+            </path>
+          </svg>
+          <h2>Keranjang kamu</h2>
+        </div>
+        <hr>
+      </div>
+    `
 
+    data.map(item, i => {
+      itemsData +=
+        `
+      <div class="flex justify-between items-end mb-4">
+        <div class="flex space-x-6">
+          <div>${i+1}</div>
+          <div class="flex flex-col items-start space-y-2">
+            <div>
+              <h4 class="text-lg">${item.name}</h4>
+              <p class="text-sm text-gray-600">Rp.${toRupiah(item.price)}</p>
+            </div>
+            <div class="flex justify-center text-center space-x-4 items-center">
+              <button class="p-1 bg-red-200 text-red-500 rounded-full">
+                <svg class="w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                    clip-rule="evenodd">
+                  </path>
+                </svg>
+              </button>
+              <h4 class="font-semibold text-md text-purple-500">${item.qty}</h4>
+              <button class="p-1 bg-green-200 text-green-500 rounded-full">
+                <svg class="w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd"
+                    d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                    clip-rule="evenodd">
+                  </path>
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="text-right font-semibold text-xl text-purple-700">Rp.${toRupiah(item.subtotal)}</div>
+      </div>
+      `
+    })
+  } else {
+    headerData +=
+      `
+      <h4 class="font-semibold text-lg text-purple-400 text-center">Tidak ada produk di keranjang</h4>
+    `
   }
+  cartHeader.innerHTML = headerData
+
   console.error(cart)
   console.error(JSON.parse(localStorage.getItem('CART')))
 }
@@ -199,6 +292,7 @@ const homeComponentActive = () => {
   btnCart.className = ''
   btnCart.classList.add('text-white')
   btnCart.classList.add('p-2')
+  totalItems.classList.add('hidden')
 }
 
 const cartComponentActive = () => {
@@ -213,8 +307,14 @@ const cartComponentActive = () => {
 
   // cart
   cartComponent.classList.remove('hidden')
+  totalItems.classList.remove('hidden')
   btnCart.classList.add('text-white')
   btnCart.classList.add('text-red-500')
   btnCart.classList.add('rounded-full')
   btnCart.classList.add('bg-red-200')
+  btnCart.classList.add('relative')
 }
+
+const toRupiah = (val) => {
+  return val.toString().replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, "$1\.")
+};
