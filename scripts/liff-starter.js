0edbf4863c4f6a7034c7622bf7766794af9693fd
Wahyu Febrianto <febriantowahyu63@gmail.com>
2020-12-26 15:19:14 +0700

    refactor

--- scripts/liff-starter.js
+++ scripts/liff-starter.js
@@ -373,21 +373,24 @@ btnOrderNow.addEventListener('click', async () => {
 			},
 
 			{
-        type: 'bubble',
-        body: {
-          type: 'box',
-          layout: 'vertical',
-          contents: [
-            {
-              type: 'text',
-              text: 'hello',
-            },
-            {
-              type: 'text',
-              text: 'world',
-            },
-          ],
-        },
+				type: 'flex',
+				altText: 'this is a flex message',
+				contents: {
+					type: 'bubble',
+					body: {
+						type: 'box',
+						layout: 'vertical',
+						contents: [
+							{
+								type: 'text',
+								text: 'hello',
+							},
+							{
+								type: 'text',
+								text: 'world',
+							},
+						],
+					},
 			},
 			// {
 			//   type:'sticker',
