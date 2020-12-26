52bd8994f5f7c48b5289b68bbb00cff662b8c477
Wahyu Febrianto <febriantowahyu63@gmail.com>
2020-12-26 21:27:40 +0700

    test flex message

--- scripts/liff-starter.js
+++ scripts/liff-starter.js
@@ -198,15 +198,15 @@ const addToCart = (id) => {
 			localStorage.setItem('CART', [JSON.stringify(cart)]);
 		}
 	}
-  loadTotalItems();
-  Swal.fire({
-    position: 'center',
-    icon: 'success',
-    title: 'Yeay!',
-    text: 'Berhasil menambahkan ke keranjang',
-    showConfirmButton: false,
-    timer: 1000
-  })
+	loadTotalItems();
+	Swal.fire({
+		position: 'center',
+		icon: 'success',
+		title: 'Yeay!',
+		text: 'Berhasil menambahkan ke keranjang',
+		showConfirmButton: false,
+		timer: 1000,
+	});
 };
 
 const addQty = (id) => {
@@ -360,172 +360,178 @@ btnOrderNow.addEventListener('click', async () => {
 
 		const sendMessage = await liff.sendMessages([
 			{
-        "type": "bubble",
-        "hero": {
-          "type": "image",
-          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-          "size": "full",
-          "aspectRatio": "20:13",
-          "aspectMode": "cover",
-          "action": {
-            "type": "uri",
-            "uri": "http://linecorp.com/"
-          }
-        },
-        "body": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "text",
-              "text": "Brown Cafe",
-              "weight": "bold",
-              "size": "xl"
-            },
-            {
-              "type": "box",
-              "layout": "baseline",
-              "margin": "md",
-              "contents": [
-                {
-                  "type": "icon",
-                  "size": "sm",
-                  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                },
-                {
-                  "type": "icon",
-                  "size": "sm",
-                  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                },
-                {
-                  "type": "icon",
-                  "size": "sm",
-                  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                },
-                {
-                  "type": "icon",
-                  "size": "sm",
-                  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                },
-                {
-                  "type": "icon",
-                  "size": "sm",
-                  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-                },
-                {
-                  "type": "text",
-                  "text": "4.0",
-                  "size": "sm",
-                  "color": "#999999",
-                  "margin": "md",
-                  "flex": 0
-                }
-              ]
-            },
-            {
-              "type": "box",
-              "layout": "vertical",
-              "margin": "lg",
-              "spacing": "sm",
-              "contents": [
-                {
-                  "type": "box",
-                  "layout": "baseline",
-                  "spacing": "sm",
-                  "contents": [
-                    {
-                      "type": "text",
-                      "text": "Place",
-                      "color": "#aaaaaa",
-                      "size": "sm",
-                      "flex": 1
-                    },
-                    {
-                      "type": "text",
-                      "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-                      "wrap": true,
-                      "color": "#666666",
-                      "size": "sm",
-                      "flex": 5
-                    }
-                  ]
-                },
-                {
-                  "type": "box",
-                  "layout": "baseline",
-                  "spacing": "sm",
-                  "contents": [
-                    {
-                      "type": "text",
-                      "text": "Time",
-                      "color": "#aaaaaa",
-                      "size": "sm",
-                      "flex": 1
-                    },
-                    {
-                      "type": "text",
-                      "text": "10:00 - 23:00",
-                      "wrap": true,
-                      "color": "#666666",
-                      "size": "sm",
-                      "flex": 5
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
-        },
-        "footer": {
-          "type": "box",
-          "layout": "vertical",
-          "spacing": "sm",
-          "contents": [
-            {
-              "type": "button",
-              "style": "link",
-              "height": "sm",
-              "action": {
-                "type": "uri",
-                "label": "CALL",
-                "uri": "https://linecorp.com"
-              }
-            },
-            {
-              "type": "button",
-              "style": "link",
-              "height": "sm",
-              "action": {
-                "type": "uri",
-                "label": "WEBSITE",
-                "uri": "https://linecorp.com"
-              }
-            },
-            {
-              "type": "spacer",
-              "size": "sm"
-            }
-          ],
-          "flex": 0
-        },
-        "styles": {
-          "header": {
-            "backgroundColor": "#"
-          }
-        }
-      }
-    ]);
-    cart = []
-    localStorage.removeItem('CART')
-    Swal.fire({
-      position: 'center',
-      icon: 'success',
-      title: 'Yeay!',
-      text: 'Berhasil melakukan pemesanan',
-      showConfirmButton: false,
-      timer: 1000
-    })
-    loadCartData();
+				type: 'bubble',
+				hero: {
+					type: 'image',
+					url:
+						'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
+					size: 'full',
+					aspectRatio: '20:13',
+					aspectMode: 'cover',
+					action: {
+						type: 'uri',
+						uri: 'http://linecorp.com/',
+					},
+				},
+				body: {
+					type: 'box',
+					layout: 'vertical',
+					contents: [
+						{
+							type: 'text',
+							text: 'Brown Cafe',
+							weight: 'bold',
+							size: 'xl',
+						},
+						{
+							type: 'box',
+							layout: 'baseline',
+							margin: 'md',
+							contents: [
+								{
+									type: 'icon',
+									size: 'sm',
+									url:
+										'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png',
+								},
+								{
+									type: 'icon',
+									size: 'sm',
+									url:
+										'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png',
+								},
+								{
+									type: 'icon',
+									size: 'sm',
+									url:
+										'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png',
+								},
+								{
+									type: 'icon',
+									size: 'sm',
+									url:
+										'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png',
+								},
+								{
+									type: 'icon',
+									size: 'sm',
+									url:
+										'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png',
+								},
+								{
+									type: 'text',
+									text: '4.0',
+									size: 'sm',
+									color: '#999999',
+									margin: 'md',
+									flex: 0,
+								},
+							],
+						},
+						{
+							type: 'box',
+							layout: 'vertical',
+							margin: 'lg',
+							spacing: 'sm',
+							contents: [
+								{
+									type: 'box',
+									layout: 'baseline',
+									spacing: 'sm',
+									contents: [
+										{
+											type: 'text',
+											text: 'Place',
+											color: '#aaaaaa',
+											size: 'sm',
+											flex: 1,
+										},
+										{
+											type: 'text',
+											text: 'Miraina Tower, 4-1-6 Shinjuku, Tokyo',
+											wrap: true,
+											color: '#666666',
+											size: 'sm',
+											flex: 5,
+										},
+									],
+								},
+								{
+									type: 'box',
+									layout: 'baseline',
+									spacing: 'sm',
+									contents: [
+										{
+											type: 'text',
+											text: 'Time',
+											color: '#aaaaaa',
+											size: 'sm',
+											flex: 1,
+										},
+										{
+											type: 'text',
+											text: '10:00 - 23:00',
+											wrap: true,
+											color: '#666666',
+											size: 'sm',
+											flex: 5,
+										},
+									],
+								},
+							],
+						},
+					],
+				},
+				footer: {
+					type: 'box',
+					layout: 'vertical',
+					spacing: 'sm',
+					contents: [
+						{
+							type: 'button',
+							style: 'link',
+							height: 'sm',
+							action: {
+								type: 'uri',
+								label: 'CALL',
+								uri: 'https://linecorp.com',
+							},
+						},
+						{
+							type: 'button',
+							style: 'link',
+							height: 'sm',
+							action: {
+								type: 'uri',
+								label: 'WEBSITE',
+								uri: 'https://linecorp.com',
+							},
+						},
+						{
+							type: 'spacer',
+							size: 'sm',
+						},
+					],
+					flex: 0,
+				},
+				styles: {
+					header: {
+						backgroundColor: '#',
+					},
+				},
+			},
+		]);
+		cart = [];
+		localStorage.removeItem('CART');
+		Swal.fire({
+			position: 'center',
+			icon: 'success',
+			title: 'Yeay!',
+			text: 'Berhasil melakukan pemesanan',
+			showConfirmButton: false,
+			timer: 1000,
+		});
+		loadCartData();
 	} catch (error) {
 		console.error(error);
 	}
