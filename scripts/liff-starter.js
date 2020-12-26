8305e790945b042077d9a80a99af1b7e9db016e2
Wahyu Febrianto <febriantowahyu63@gmail.com>
2020-12-26 22:05:54 +0700

    test flex message

--- scripts/liff-starter.js
+++ scripts/liff-starter.js
@@ -364,41 +364,6 @@ btnOrderNow.addEventListener('click', async () => {
 				text: `Hai ${userData.name}, \nTerimakasih telah memesan minuman di EXA BOBA\n \nBerikut rincian pesanannya : \n\n${itemData}Total : Rp.${toRupiah(totals)} \n\nPesanan akan segera diproses dan mohon untuk ditunggu ya😊`,
 			},
 			{
-				type: 'flex',
-				altText: 'this is a flex message',
-				contents: {
-					type: 'bubble',
-					body: {
-						type: 'box',
-						layout: 'vertical',
-						contents: [
-							{
-								type: 'text',
-								text: 'hello',
-							},
-							{
-								type: 'text',
-								text: 'world',
-							},
-						],
-					},
-					footer: {
-						type: 'box',
-						layout: 'horizontal',
-						contents: [
-							{
-								type: 'text',
-								text: 'Footer 1',
-							},
-							{
-								type: 'text',
-								text: 'Footer 2',
-							},
-						],
-					},
-				},
-			},
-			{
 				type: 'carousel',
 				contents: [
 					{
@@ -442,6 +407,7 @@ btnOrderNow.addEventListener('click', async () => {
 		});
 		loadCartData();
 	} catch (error) {
+    alert(error)
 		console.error(error);
 	}
 });
