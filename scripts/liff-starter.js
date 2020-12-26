4be66e194eb0baa9f7f493063f3e1867a84cb026
Wahyu Febrianto <febriantowahyu63@gmail.com>
2020-12-26 15:10:03 +0700

    refactor

--- scripts/liff-starter.js
+++ scripts/liff-starter.js
@@ -383,10 +383,6 @@ btnOrderNow.addEventListener('click', async () => {
 						},
 					],
 				},
-				hero: {
-					type: 'image',
-					url: 'https://i.mydramalist.com/BDBgq_4c.jpg',
-				},
 				body: {
 					type: 'box',
 					layout: 'vertical',
@@ -410,7 +406,7 @@ btnOrderNow.addEventListener('click', async () => {
 				styles: {
 					comment: 'See the example of a bubble style object',
 				},
-			},
+			}
 			// {
 			//   type:'sticker',
 			//   packageId:'11537',
